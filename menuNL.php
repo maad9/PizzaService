bfb63ded2a82f2fef3dd1da3fc75ce5114984b23
Adam <adam.wolanski94@gmail.com>
2015-05-19 13:07:17 +0200

    Create menuNL.php

---
+++ menuNL.php
@@ -0,0 +1,21 @@
+<div id="menu">
+	<div class="option">
+		<a href="index.php" style="text-decoration:none; color:white">Strona główna</a>
+	</div>
+	<div class="option">
+		<a href=".php" style="text-decoration:none; color:white">Forum</a>
+	</div>
+	<div class="option">
+		<a href="download.php" style="text-decoration:none; color:white">Download</a>
+	</div>			
+	<div class="option" >
+		<a href="about.php" style="text-decoration:none; color:white">O autorze</a>
+	</div>			 
+	<div class="option">
+		<a href="login.php" style="text-decoration:none; color:white">Zaloguj</a>
+	</div>
+	<div class="option">
+		<a href=".php" style="text-decoration:none; color:white">Zarejestruj</a>
+	</div>
+	<div style="clear:both"></div>
+</div>
