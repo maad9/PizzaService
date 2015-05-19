6c4b18351ce623b41d29ba09c0e3badc0d6a6b6f
Adam <adam.wolanski94@gmail.com>
2015-05-19 13:07:00 +0200

    Create menuL.php

---
+++ menuL.php
@@ -0,0 +1,21 @@
+<div id="menu">
+	<div class="option">
+		<a href="index.php" style="text-decoration:none; color:white">Strona główna</a>
+	</div>
+	<div class="option">
+		<a href=".php" style="text-decoration:none; color:white">Forum</a>
+	</div>
+	<div class="option">
+		<a href="download.php" style="text-decoration:none; color:white">Download</a>
+	</div>			
+	<div class="option" >
+		<a href="about.php" style="text-decoration:none; color:white">O autorze</a>
+	</div>			 
+	<div class="option">
+		<a href="panel.php" style="text-decoration:none; color:white">Panel gracza</a>
+	</div>
+	<div class="option">
+		<a href="logout.php" style="text-decoration:none; color:white">Wyloguj <?php echo $_SESSION['login']; ?></a>
+	</div>
+	<div style="clear:both"></div>
+</div>
